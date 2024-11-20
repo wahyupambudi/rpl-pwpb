c4d678fc45af523994151596949c7a423475cdb3
wahyupambudi <wahyupambudi823@gmail.com>
2024-11-20 09:21:00 +0700

    Add files via upload

---
+++ admin/index.php
@@ -0,0 +1,140 @@
+<?php
+
+session_start();
+include '../koneksi.php';
+
+if(isset($_SESSION['role']) && $_SESSION['role'] === 'admin') { ?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title>Dashboard Admin</title>
+
+    <!-- Custom fonts for this template-->
+    <link href="vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
+    <link
+        href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i"
+        rel="stylesheet">
+
+    <!-- Custom styles for this template-->
+    <link href="css/sb-admin-2.min.css" rel="stylesheet">
+
+    <!-- Custom styles for this page -->
+    <link href="vendor/datatables/dataTables.bootstrap4.min.css" rel="stylesheet">
+
+</head>
+
+<body id="page-top">
+
+    <!-- Page Wrapper -->
+    <div id="wrapper">
+
+        <?php include 'nav.php' ?>
+
+        <!-- Content Wrapper -->
+        <div id="content-wrapper" class="d-flex flex-column">
+
+            <!-- Main Content -->
+            <div id="content">
+
+                <?php include 'topbar.php' ?>
+
+                <!-- Begin Page Content -->
+                <div class="container-fluid">
+                
+                    <?php 
+                      if(isset($_GET['page'])) {
+                        $page = $_GET['page'];
+                        switch ($page) {
+                          case 'siswa':
+                            include "data/siswa.php";
+                            break;
+                          case 'user':
+                            include 'data/user.php';
+                            break;
+                          case 'mapel':
+                            include 'data/mapel.php';
+                            break;
+                          case 'absen':
+                            include 'data/absen.php';
+                            break;
+                          default:
+                            echo "<center> <h3> Halaman tidak ditemukan! </h3> </center>";
+                            break;
+                        }
+                      } else {
+                        include "data/dashboard.php";
+                      }
+                    ?>
+
+                </div>
+                <!-- /.container-fluid -->
+
+            </div>
+            <!-- End of Main Content -->
+
+            <!-- Footer -->
+            <footer class="sticky-footer bg-white">
+                <div class="container my-auto">
+                    <div class="copyright text-center my-auto">
+                        <span>Copyright &copy; UKK RPL WAHYU 2024</span>
+                    </div>
+                </div>
+            </footer>
+            <!-- End of Footer -->
+
+        </div>
+        <!-- End of Content Wrapper -->
+
+    </div>
+    <!-- End of Page Wrapper -->
+
+    <!-- Scroll to Top Button-->
+    <a class="scroll-to-top rounded" href="#page-top">
+        <i class="fas fa-angle-up"></i>
+    </a>
+
+    
+
+    <!-- Bootstrap core JavaScript-->
+    <script src="vendor/jquery/jquery.min.js"></script>
+    <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+
+    <!-- Core plugin JavaScript-->
+    <script src="vendor/jquery-easing/jquery.easing.min.js"></script>
+
+    <!-- Custom scripts for all pages-->
+    <script src="js/sb-admin-2.min.js"></script>
+
+    <!-- Page level plugins -->
+    <script src="vendor/datatables/jquery.dataTables.min.js"></script>
+    <script src="vendor/datatables/dataTables.bootstrap4.min.js"></script>
+
+    <!-- Page level custom scripts -->
+    <script src="js/demo/datatables-demo.js"></script>
+
+</body>
+
+</html>
+
+<?php
+
+ } else { ?>
+
+    <script> 
+      alert('Anda tidak memiliki akses'); 
+      window.location.href = '/pwpb';
+    </script> 
+
+ <?php
+ }
+
+ ?>
---
+++ admin/nav.php
@@ -0,0 +1,53 @@
+<!-- Sidebar -->
+<ul class="navbar-nav bg-gradient-primary sidebar sidebar-dark accordion" id="accordionSidebar">
+    <!-- Sidebar - Brand -->
+    <a class="sidebar-brand d-flex align-items-center justify-content-center" href="index.html">
+        <div class="sidebar-brand-icon rotate-n-15">
+            <i class="fas fa-laugh-wink"></i>
+        </div>
+        <div class="sidebar-brand-text mx-3">SB Admin <sup>2</sup></div>
+    </a>
+    <!-- Divider -->
+    <hr class="sidebar-divider my-0">
+    <!-- Nav Item - Dashboard -->
+    <li class="nav-item active">
+        <a class="nav-link" href="../admin">
+            <i class="fas fa-fw fa-tachometer-alt"></i>
+            <span>Dashboard</span></a>
+        </li>
+        <!-- Divider -->
+        <hr class="sidebar-divider">
+        <!-- Data Siswa -->
+        <div class="sidebar-heading">
+            Data
+        </div>
+        <!-- Nav Item - Tables -->
+        <li class="nav-item">
+            <a class="nav-link" href="?page=siswa">
+                <i class="fas fa-fw fa-table"></i>
+                <span>Data Siswa</span>
+            </a>
+        </li>
+        <!-- Nav Item - Tables -->
+        <li class="nav-item">
+            <a class="nav-link" href="?page=user">
+                <i class="fas fa-fw fa-table"></i>
+                <span>Data User</span>
+            </a>
+        </li>
+        <!-- Nav Item - Tables -->
+        <li class="nav-item">
+            <a class="nav-link" href="?page=mapel">
+                <i class="fas fa-fw fa-table"></i>
+                <span>Data Mapel</span>
+            </a>
+        </li>
+        <!-- Nav Item - Tables -->
+        <li class="nav-item">
+            <a class="nav-link" href="?page=absen">
+                <i class="fas fa-fw fa-table"></i>
+                <span>Data Absensi</span>
+            </a>
+        </li>
+    </ul>
+    <!-- End of Sidebar -->
